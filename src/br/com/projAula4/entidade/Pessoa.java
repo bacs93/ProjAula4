0277e49825ba24fcd801f178b76bde49eb3a488d
bacs93 <aluno@fiar.local>
2018-02-28 19:28:21 -0300

    Proj4

---
+++ src/br/com/projAula4/entidade/Pessoa.java
@@ -0,0 +1,28 @@
+package br.com.projAula4.entidade;
+
+public class Pessoa {
+	
+	//Atributos
+	
+	private int id;
+	private String nome;
+	
+	//Métodos de Acesso
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+
+}
